0743eaf606875ed6c3a087f2a3268fcbc18609af
syarifh-id <125syarifh@gmail.com>
2023-08-26 20:08:07 +0700

    data types

---
+++ 3. Data Types/7. Boolean.php
@@ -0,0 +1,7 @@
+<?php
+$var = false; // $var is false.
+$var = 1; // $var is true.
+$var = -1; // $var is true.
+$var = 5; // $var is true.
+$var = 0; // $var is false.
+?>
---
+++ 3. Data Types/9. Array.php
@@ -0,0 +1,14 @@
+<?php
+$array = array(
+    "foo" => "bar",
+    "bar" => "foo",
+);
+
+$arrays = array(
+    1    => "a",
+    "1"  => "b",
+    1.5  => "c",
+    true => "d",
+);
+var_dump($arrays);
+?>
