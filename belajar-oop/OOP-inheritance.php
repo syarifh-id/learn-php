8742efcd3a6bda93928172cd562de52f8d6435c8
syarifh-id <125syarifh@gmail.com>
2024-01-25 06:52:19 +0700

    overriding to accer paternt's method

--- belajar-oop/OOP-inheritance.php
+++ belajar-oop/OOP-inheritance.php
@@ -14,7 +14,15 @@ class Buku
 		$this->penerbit = $penerbit;
 		$this->genre = $genre;
 	}
+
+	public function getDetails()
+	{
+		return "judul : {$this->judul}\npenerbit : {$this->penerbit}\npenulis : {$this->penulis}\ngenre : {$this->genre}\n";
+	}
 };
+
+
+
 //Novel is child from Buku, so it can use property from buku
 class Novel extends Buku
 {
@@ -25,8 +33,18 @@ class Novel extends Buku
 	}
 };
 
-
+class Biograpy extends Buku
+{
+	public function getDetail() //overriding to access parent's method
+	{
+		return "Detail Buku Yang anda pilih \n" . parent::getDetails();
+	}
+}
 
 
 $myNovel = new Novel('senja berlari', 'hidayat', 'Xcelent ltd', 'action');
 print($myNovel->getInfoPenulis());
+
+$myBiograpy = new Biograpy('perjalanan OWP', 'syarif', 'mediaku', 'romance');
+
+print($myBiograpy->getDetail());
