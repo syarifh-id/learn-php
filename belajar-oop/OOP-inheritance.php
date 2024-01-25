676dff18fdb9ae20b8c46fb9e906b173a62a4c68
syarifh-id <125syarifh@gmail.com>
2024-01-25 08:08:30 +0700

    abstractin the parent class

--- belajar-oop/OOP-inheritance.php
+++ belajar-oop/OOP-inheritance.php
@@ -5,19 +5,17 @@ class Buku
 	public $judul;
 	public $penulis;
 	public $penerbit;
-	public $genre;
 
-	public function __construct($judul, $penulis, $penerbit, $genre)
+	public function __construct($judul, $penulis, $penerbit)
 	{
 		$this->judul = $judul;
 		$this->penulis = $penulis;
 		$this->penerbit = $penerbit;
-		$this->genre = $genre;
 	}
 
 	public function getDetails()
 	{
-		return "judul : {$this->judul}\npenerbit : {$this->penerbit}\npenulis : {$this->penulis}\ngenre : {$this->genre}\n";
+		return "judul : {$this->judul}\npenerbit : {$this->penerbit}\npenulis : {$this->penulis}\n";
 	}
 };
 
@@ -26,25 +24,58 @@ class Buku
 //Novel is child from Buku, so it can use property from buku
 class Novel extends Buku
 {
+	public $genre;
+
+	public function __construct($judul, $penulis, $penerbit, $genre)
+	{
+		parent::__construct($judul, $penulis, $penerbit);
+		$this->genre = $genre;
+	}
+
 	public function getInfoPenulis()
 	{
 
 		return "Buku {$this->judul} ditulis oleh : {$this->penulis} \n";
 	}
+
+	public function getInfoGenre()
+	{
+		return "Buku {$this->judul} bergenre {$this->genre}\n";
+	}
+
+	public function getDetail() //overriding to access parent's method
+	{
+		return "Detail Buku Yang anda pilih \n" . parent::getDetails() . "genre : {$this->genre}\n";
+	}
 };
+
+
+
+// another class child of Buku
 
 class Biograpy extends Buku
 {
-	public function getDetail() //overriding to access parent's method
+	public $figure;
+
+	public function __construct($judul, $penulis, $penerbit, $figure)
 	{
-		return "Detail Buku Yang anda pilih \n" . parent::getDetails();
+		parent::__construct($judul, $penulis, $penerbit,);
+		$this->figure = $figure;
+	}
+
+	public function getFigure()
+	{
+		return "buku biografi berjudul : {$this->judul} menceritakan seorang {$this->figure}";
 	}
 }
 
 
+
+
 $myNovel = new Novel('senja berlari', 'hidayat', 'Xcelent ltd', 'action');
 print($myNovel->getInfoPenulis());
+print($myNovel->getInfoGenre());
+print($myNovel->getDetail());
 
-$myBiograpy = new Biograpy('perjalanan OWP', 'syarif', 'mediaku', 'romance');
-
-print($myBiograpy->getDetail());
+$myBiograpy = new Biograpy('perjalanan OWP', 'syarif', 'mediaku', 'Pejuang Internet Indonesia');
+print($myBiograpy->getFigure());
