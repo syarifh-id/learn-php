9bf3cff7a60e2ca5e20211d042e3c8f626e6d419
syarifh-id <125syarifh@gmail.com>
2025-03-07 09:18:38 +0700

    tugas kuliah semester 1

---
+++ tugas-kuliah/diodata.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata Mahasiswa</title>
+</head>
+<body>
+<?php 
+$nama = "Aya Majnun";
+$alamat = "Jl. Kapten Tendean No. 55, Yogyakarta";
+$ttl = "Yogyakarta, 20 Oktober 2010";
+$tlp = "081325498756";
+?>
+
+<p>Nama : <?php echo $nama; ?></p>
+<p>Alamat : <?php echo $alamat; ?></p>
+<p>Tanggal Lahir : <?php echo $ttl; ?></p>
+<p>No Telepon : <?php echo $tlp; ?></p>
+
+</body>
+</html>
